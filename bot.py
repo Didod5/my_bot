11adf487edff32c3e6d4b57b74966f202fc199f9
Didod5 <didod1020@gmail.com>
2023-05-29 14:26:55 +0400

    bit edited

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
-
 import settings
+import ephem
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
@@ -15,12 +15,19 @@ def talk_to_me(update, context):
     print(text)
     update.message.reply_text(text)
 
+def get_coord(update, context):
+    planet = update.message.text.split()[-1]
+    if planet == 'mars' or 'Mars':
+        const = ephem.constellation(ephem.Mars('2023/05/05'))
+        update.message.reply_text(const)
+
 def main():
     mybot = Updater(settings.API_KEY)
     
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    # dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler("planet", get_coord))
 
 
     logging.info('Bot started')
